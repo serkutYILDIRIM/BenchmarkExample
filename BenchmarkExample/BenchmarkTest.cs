b2fb60fb13dcd8ac36cfb3575ebb55c969457437
serkutYILDIRIM <serkutyldrm@gmail.com>
2023-11-18 12:11:46 +0300

    Benchmark tests added

---
+++ BenchmarkExample/BenchmarkTest.cs
@@ -0,0 +1,39 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace BenchmarkExample
+{
+    [MemoryDiagnoser(false)]
+    public class BenchmarkTest
+    {
+
+        [Benchmark]
+        public int FindMaxNumberWithOldWay()
+        {
+            var numbers = new int[1000];
+
+            for (int i = 0; i < numbers.Length; i++)
+                numbers[i] = i;
+
+            //Old Way Method Syntax
+            var maxNumber = numbers.OrderByDescending(x => x).FirstOrDefault();
+
+            return maxNumber;
+        }
+
+        [Benchmark]
+        public int MaxByMethod()
+        {
+            var numbers = new int[1000];
+
+            for (int i = 0; i < numbers.Length; i++)
+                numbers[i] = i;
+
+            //MaxBy Method Syntax
+            var maxNumber = numbers.MaxBy(x => x);
+
+            return maxNumber;
+        }
+    }
+}
+
+
--- BenchmarkExample/Program.cs
+++ BenchmarkExample/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using BenchmarkDotNet.Running;
+using BenchmarkExample;
+
+var summary = BenchmarkRunner.Run<BenchmarkTest>();
